57ad7ab1dbdea72b1ecb116e509c1b4c82725be8
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-08-06 17:38:40 +0300

    feat: add fields to User model: created_at, is_email_verified, phone_number, and updated_at

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import AbstractUser
 class User (AbstractUser):
 
     email=models.EmailField(max_length=255, unique=True) 
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    is_email_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
